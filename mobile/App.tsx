fd1500f2c05d520e5dafc8fa895fe98d682887b0
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-01 14:14:38 -0300

    mobile - status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { Center, NativeBaseProvider, Text } from "native-base";
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
 import { THEME } from './src/styles/theme'
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from './src/components/Loading'
+import { SignIn } from './src/screens/Signin'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor={'gray.900'} >
-        <Text color='white' fontSize={24}>
-          Hello React Native
-        </Text>
-        <StatusBar style="auto" />
-      </ Center>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
-  );
+  )
 }
