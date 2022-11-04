8031e45c1ffe0de03f447cb4bd28b16e5bc4d217
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-03 23:09:16 -0300

    Button Component and Signin

--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,11 +1,25 @@
-import { Center, Text } from 'native-base'
+import { Center, Text, Icon } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
 
-export function SignIn(){
-  return(
-    <Center flex={1} bgColor={'gray.900'} >
-      <Text color='white' fontSize={24}>
-        SignIn
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button'
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor={'gray.900'} p={7}>
+      <Logo width={212} height={40} />
+
+      <Button
+        type="SECONDARY"
+        title="ENTRAR COM O GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} de seu e-mail para criação
+        de sua conta.
       </Text>
-    </ Center>
+    </Center>
   )
 }
