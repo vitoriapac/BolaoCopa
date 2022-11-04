1506f557bee743c661379a488315f9d86f176308
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-04 02:16:35 -0300

    token auth google

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState, useEffect } from 'react'
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+
+WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
   name: string
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps
+  isUserLoading: boolean
   signIn: () => Promise<void>
 }
 
@@ -17,18 +23,47 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false)
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      '737110997274-elhj3jccv4t488bl7pkpg41hl0fdrtt6.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  })
+
+  // console.log(AuthSession.makeRedirectUri({ useProxy: true }))
+
   async function signIn() {
-    console.log('signIn called')
+    // console.log('signIn called')
+    try {
+      setIsUserLoading(true)
+      await promptAsync()
+    } catch (error) {
+      console.log('Error: ' + error)
+      throw error
+    } finally {
+      setIsUserLoading(false)
+    }
   }
+
+  async function signWithGoogle(access_token: string) {
+    console.log('Token generated', access_token)
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: 'Luis Gilbert',
-          avatarUrl: 'https://xsgames.co/randomusers/assets/avatars/male/67.jpg'
-        }
+        isUserLoading,
+        user
       }}
     >
       {children}
--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -8,8 +8,6 @@ import { Button } from '../components/Button'
 export function SignIn() {
   const { signIn, user } = userAuth()
 
-  console.log(user)
-
   return (
     <Center flex={1} bgColor={'gray.900'} p={7}>
       <Logo width={212} height={40} />
